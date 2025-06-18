b1d3a1785defc76690e3acd56ff8154cb027a955
Navid Ibrahim <nibrahim7787@gmail.com>
2025-06-18 21:25:18 +0600

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module org.example.co_po_assessment {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+
+    opens org.example.co_po_assessment to javafx.fxml;
+    exports org.example.co_po_assessment;
+}

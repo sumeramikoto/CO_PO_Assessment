366edd7073c0f9ae68204bc138c1e56cd0d21fbe
Navid Ibrahim <nibrahim7787@gmail.com>
2025-06-30 04:10:20 +0600

    added co and po attributes

--- src/main/java/org/example/co_po_assessment/excelTemplate/Question.java
+++ src/main/java/org/example/co_po_assessment/excelTemplate/Question.java
@@ -3,8 +3,18 @@ package org.example.co_po_assessment.excelTemplate;
 public class Question {
     String number;
     double maxMark;
+    String co;
+    String po;
+
     Question(String number, double maxMark) {
         this.number = number;
         this.maxMark = maxMark;
+    }
+
+    public Question(String number, double maxMark, String co, String po) {
+        this.number = number;
+        this.maxMark = maxMark;
+        this.co = co;
+        this.po = po;
     }
 }

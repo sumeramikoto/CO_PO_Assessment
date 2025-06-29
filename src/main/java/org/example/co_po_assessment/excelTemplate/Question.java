59452cff2b3d8fdedc0a9a6cdba023385f221b20
Navid Ibrahim <nibrahim7787@gmail.com>
2025-06-29 21:19:05 +0600

    object representing a question

---
+++ src/main/java/org/example/co_po_assessment/excelTemplate/Question.java
@@ -0,0 +1,10 @@
+package org.example.co_po_assessment.excelTemplate;
+
+public class Question {
+    String number;
+    double maxMark;
+    Question(String number, double maxMark) {
+        this.number = number;
+        this.maxMark = maxMark;
+    }
+}

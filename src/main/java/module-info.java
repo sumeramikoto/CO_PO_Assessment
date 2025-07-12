5a3735588599cb18fd52fb4cbaf8c9b1bb7342ec
Navid Ibrahim <nibrahim7787@gmail.com>
2025-07-13 00:28:39 +0600

    added apache poi to module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,6 +8,8 @@ module org.example.co_po_assessment {
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires eu.hansolo.tilesfx;
+    requires org.apache.poi.poi;
+    requires org.apache.poi.ooxml;
 
     opens org.example.co_po_assessment to javafx.fxml;
     exports org.example.co_po_assessment;

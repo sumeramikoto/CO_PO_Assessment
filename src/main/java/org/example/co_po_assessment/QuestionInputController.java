788645e15f8cd731f791fb0f402c7a97aba4482a
Navid Ibrahim <nibrahim7787@gmail.com>
2025-08-16 23:25:03 +0600

    removed exam radio buttons & replaced w choicebox

--- src/main/java/org/example/co_po_assessment/QuestionInputController.java
+++ src/main/java/org/example/co_po_assessment/QuestionInputController.java
@@ -4,22 +4,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
+import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class QuestionInputController implements Initializable {
     @FXML
-    RadioButton quiz1RB;
-    @FXML
-    RadioButton quiz2RB;
-    @FXML
-    RadioButton quiz3RB;
-    @FXML
-    RadioButton quiz4RB;
-    @FXML
-    RadioButton midRB;
-    @FXML
-    RadioButton finalRB;
+    ChoiceBox<String> examChoiceBox;
     @FXML
     Label examLabel;
     @FXML
@@ -49,10 +40,30 @@ public class QuestionInputController implements Initializable {
     private final String[] poArray = {"PO1","PO2","PO3","PO4","PO5","PO6","PO7","PO8","PO9","PO10",
                                 "PO11","PO12"};
 
+    private final String[] exams = {"Quiz 1", "Quiz 2", "Quiz 3", "Quiz 4", "Mid", "Final"};
+
+    String examType;
+    String questionNo;
+    int marks;
+    String co;
+    String po;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         coChoiceBox.getItems().addAll(coArray);
         poChoiceBox.getItems().addAll(poArray);
+        examChoiceBox.getItems().addAll(exams);
+    }
+
+    public void addQuestion(ActionEvent event) {
+        try {
+            questionNo = questionNoTextField.getText();
+            marks = Integer.parseInt(marksTextField.getText());
+            co = coChoiceBox.getValue();
+            po = poChoiceBox.getValue();
+            examType = examChoiceBox.getValue();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }

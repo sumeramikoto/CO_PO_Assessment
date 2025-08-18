275e4706b90da31af3f26a9c21ee20d9ee8a7702
Navid Ibrahim <nibrahim7787@gmail.com>
2025-08-18 20:20:14 +0600

    Updated question input window, fixed back button action listener and integrated with manual window

--- src/main/java/org/example/co_po_assessment/QuestionInputWindow.java
+++ src/main/java/org/example/co_po_assessment/QuestionInputWindow.java
@@ -11,7 +11,7 @@ public class QuestionInputWindow extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("questionInput-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 345, 380);
         stage.setTitle("Input Question Info");
         stage.setScene(scene);
         stage.show();

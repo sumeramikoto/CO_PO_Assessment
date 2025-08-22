809c597bb6313b9964abbb38366e13eb6998fa14
Navid Ibrahim <nibrahim7787@gmail.com>
2025-08-22 21:15:42 +0600

    fixed wrong reference

--- src/main/java/org/example/co_po_assessment/ManageFacultiesView.java
+++ src/main/java/org/example/co_po_assessment/ManageFacultiesView.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class ManageFacultiesView extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("manageFaculties-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ManageFacultiesView.class.getResource("manageFaculties-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Manage Faculty Information");
         stage.setScene(scene);

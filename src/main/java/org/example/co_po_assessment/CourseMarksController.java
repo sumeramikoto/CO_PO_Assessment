55c7a6b6d112fcc487cf4b3e0023e489051d2314
Navid Ibrahim <nibrahim7787@gmail.com>
2025-08-30 14:20:24 +0600

    added method names

--- src/main/java/org/example/co_po_assessment/CourseMarksController.java
+++ src/main/java/org/example/co_po_assessment/CourseMarksController.java
@@ -1,4 +1,23 @@
 package org.example.co_po_assessment;
 
+import javafx.event.ActionEvent;
+
 public class CourseMarksController {
+    public void onQ1BackButton(ActionEvent actionEvent) {
+    }
+
+    public void onQ2BackButton(ActionEvent actionEvent) {
+    }
+
+    public void onQ3BackButton(ActionEvent actionEvent) {
+    }
+
+    public void onQ4BackButton(ActionEvent actionEvent) {
+    }
+
+    public void onMidBackButton(ActionEvent actionEvent) {
+    }
+
+    public void onFinalBackButton(ActionEvent actionEvent) {
+    }
 }

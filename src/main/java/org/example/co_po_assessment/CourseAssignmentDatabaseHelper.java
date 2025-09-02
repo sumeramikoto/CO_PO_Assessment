a2e25d598b40e6e05475956525636b9312e79f75
SinhaWiz <sinhartx2070@gmail.com>
2025-09-02 20:11:41 +0600

    course and course assignment updated properly

--- src/main/java/org/example/co_po_assessment/CourseAssignmentDatabaseHelper.java
+++ src/main/java/org/example/co_po_assessment/CourseAssignmentDatabaseHelper.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class CourseAssignmentDatabaseHelper {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/SPL2";
     private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "pass";
+    private static final String DB_PASSWORD = "sinhawiz123";
 
     private Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
--- src/main/java/org/example/co_po_assessment/DatabaseService.java
+++ src/main/java/org/example/co_po_assessment/DatabaseService.java
@@ -10,7 +10,7 @@ public class DatabaseService {
     // ------------------------------------------------------------------
     private static final String DB_URL = "jdbc:mysql://localhost:3306/SPL2";
     private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "pass";
+    private static final String DB_PASSWORD = "sinhawiz123";
     private static DatabaseService instance;
 
     private DatabaseService() {

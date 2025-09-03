1e2981453f01ee2f593c2b16e286318bcb1e54a9
Navid Ibrahim <nibrahim7787@gmail.com>
2025-09-03 07:29:04 +0600

    renamed AdminDashboardWindow to AdminDashboardView

--- src/main/java/org/example/co_po_assessment/AdminDashboardView.java
+++ src/main/java/org/example/co_po_assessment/AdminDashboardView.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class AdminDashboardWindow extends Application {
+public class AdminDashboardView extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("adminDashboard-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(AdminDashboardView.class.getResource("adminDashboard-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Admin Dashboard");
         stage.setScene(scene);
